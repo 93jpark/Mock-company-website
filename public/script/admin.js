fe355b1d38ead8e6456daa1a3a555faf409ab1a2
93jpark <93jpark@gmail.com>
2019-12-02 01:00:53 -0500

    no message

--- public/script/admin.js
+++ public/script/admin.js
@@ -1,9 +1,45 @@
+var database = [];
+
 function add() {
     var name = document.querySelector("#foodName").value;
     var price = document.querySelector("#price").value;
     var qty = document.querySelector("#qty").value;
+
     if(validate()){
-        document.querySelector(".table").innerHTML += "hello";
+        var item = {
+            name: name,
+            price: price,
+            qty: qty
+        };
+
+        database.push(item);
+        var str = "<div class=\"row\">";
+        str += "<div class=\"col\">";
+        str += "Name";
+        str += "</div>";
+        str += "<div class=\"col\">";
+        str += "Price";
+        str += "</div>";
+        str += "<div class=\"col\">";
+        str += "Qty";
+        str += "</div>";
+        str += "</div>";
+        document.querySelector(".table").innerHTML=str;
+
+        update();
+        // var str = "<div class=\"row\">";
+        // str += "<div class=\"col\">";
+        // str += name;
+        // str += "</div>";
+        // str += "<div class=\"col\">";
+        // str += price;
+        // str += "</div>";
+        // str += "<div class=\"col\">";
+        // str += qty;
+        // str += "</div>";
+        // str += "</div>";
+        // str += "\n";
+        //document.querySelector(".table").innerText += str;
     }
 }
 
@@ -14,4 +50,27 @@ function validate() {
         return false;
     }
     return true;
+}
+
+function update() {
+    database.forEach(function(item){
+        var str = "<div class=\"row\">";
+        str += "<div class=\"col\">";
+        str += item.name;
+        str += "</div>";
+        str += "<div class=\"col\">";
+        str += item.price;
+        str += "</div>";
+        str += "<div class=\"col\">";
+        str += item.qty;
+        str += "</div>";
+        str += "</div>";
+        // var str = "";
+        // str += item.name;
+        // str += ", "+item.price;
+        // str += " : "+item.qty+"<br>";
+        console.log(str);
+        document.querySelector(".table").innerHTML += str;
+    });
+
 }
