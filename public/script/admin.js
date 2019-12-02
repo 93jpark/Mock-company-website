8dd30c3b5b2c89570daee8fc45505172353cb78f
93jpark <93jpark@gmail.com>
2019-12-02 14:45:59 -0500

    no message

--- public/script/admin.js
+++ public/script/admin.js
@@ -11,35 +11,8 @@ function add() {
             price: price,
             qty: qty
         };
-
         database.push(item);
-        var str = "<div class=\"row\">";
-        str += "<div class=\"col\">";
-        str += "Name";
-        str += "</div>";
-        str += "<div class=\"col\">";
-        str += "Price";
-        str += "</div>";
-        str += "<div class=\"col\">";
-        str += "Qty";
-        str += "</div>";
-        str += "</div>";
-        document.querySelector(".table").innerHTML=str;
-
         update();
-        // var str = "<div class=\"row\">";
-        // str += "<div class=\"col\">";
-        // str += name;
-        // str += "</div>";
-        // str += "<div class=\"col\">";
-        // str += price;
-        // str += "</div>";
-        // str += "<div class=\"col\">";
-        // str += qty;
-        // str += "</div>";
-        // str += "</div>";
-        // str += "\n";
-        //document.querySelector(".table").innerText += str;
     }
 }
 
@@ -53,6 +26,19 @@ function validate() {
 }
 
 function update() {
+    document.querySelector(".btn-sort").style.visibility = "visible";
+    var str = "<div class=\"row\">";
+    str += "<div class=\"col\">";
+    str += "Name";
+    str += "</div>";
+    str += "<div class=\"col\">";
+    str += "Price";
+    str += "</div>";
+    str += "<div class=\"col\">";
+    str += "Qty";
+    str += "</div>";
+    str += "</div>";
+    document.querySelector(".table").innerHTML=str;
     database.forEach(function(item){
         var str = "<div class=\"row\">";
         str += "<div class=\"col\">";
@@ -65,12 +51,45 @@ function update() {
         str += item.qty;
         str += "</div>";
         str += "</div>";
-        // var str = "";
-        // str += item.name;
-        // str += ", "+item.price;
-        // str += " : "+item.qty+"<br>";
-        console.log(str);
         document.querySelector(".table").innerHTML += str;
     });
+    document.querySelector("#foodName").value = "";
+    document.querySelector("#price").value = 1;
+    document.querySelector("#qty").value = 0;
+}
 
+function nameSort() {
+    
+    database.sort(dynamicSort("name"));
+    update()
+
+     function dynamicSort(property) {
+        var sortOrder = 1;
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+        return function (a,b) {
+            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortOrder;
+        }
+    }
+}
+
+function priceSort() {
+    
+    database.sort(dynamicSort("price"));
+    update()
+
+     function dynamicSort(property) {
+        var sortOrder = 1;
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+        return function (a,b) {
+            var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortOrder;
+        }
+    }
 }
